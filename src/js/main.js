8682c0861df9e1f84fd88c22111fd001823a8297
Jakub Bobiński <bobinski.jakub2@gmail.com>
2021-10-06 20:09:05 +0200

    init

--- src/js/main.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-    document.getElementById("main-nav").style.height = "80px";
-  } else {
-    document.getElementById("main-nav").style.height = "100px";
-  }
-}
